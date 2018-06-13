57e7424f72dc43cf9843704a87be002c3f7e53c6
Admin <Admin@DESKTOP-H08TIEQ>
2018-06-13 17:45:08 +0300

    first comit

--- lab2_0_2/Controllers/HomeController.cs
+++ lab2_0_2/Controllers/HomeController.cs
@@ -12,8 +12,43 @@ namespace lab2_0_2.Controllers
         {
             return View();
         }
+    
 
-        public ActionResult About()
+    [HttpPost]
+    public ActionResult Index(string firstNum, string secondNum, String calc)
+    {
+
+        double a;
+        double b;
+        double.TryParse(secondNum, out a);
+        double.TryParse(firstNum, out b);
+        double c = 0;
+
+        switch (calc)
+        {
+            case "+":
+                c = a + b;
+                break;
+            case "-":
+                c = a - b;
+                break;
+            case "*":
+                c = a * b;
+                break;
+            case "/":
+                c = a / b;
+                break;
+        }
+        b = c;
+
+        ViewBag.secondNum = b;
+        ViewBag.Result = c;
+
+        return View();
+
+    }
+
+    public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
